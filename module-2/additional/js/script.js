59f2d8dcc41cd3f0911e6070fff5884eaaf4fdfa
Remas Ulianytskiy <remas.omar.101@gmail.com>
2018-02-15 21:27:53 +0200

    add HW module-2

--- module-2/additional/js/script.js
+++ module-2/additional/js/script.js
@@ -1,12 +1,49 @@
-let taba = 6;
-let sharm = 15;
-let hurgada = 25;
+function main() {
+  const numberMemmbers = parseFloat(prompt("Enter the number of group members"));
 
-const numberMemmbers = prompt('Enter the number of group members');
+  let taba = 6;
+  let sharm = 15;
+  let hurgada = 25;
 
-if (numberMemmbers <= taba) {
-  confirm(`There are empty seats. Do you want to add?`)
-  if (true) {
-    
+  if (Number.isInteger(numberMemmbers) === true && numberMemmbers > 0) {
+
+    if (numberMemmbers <= taba) {
+      let question = confirm('There are empty seats in Taba. Do you want to add?');
+      if (question == true) {
+        alert('You add to Taba group');
+        taba = taba - numberMemmbers;
+        console.log(`Free Places in Taba: ${taba}`);
+        return;
+      }
+    }
+
+    if (numberMemmbers <= sharm) {
+      let question = confirm('There are empty seats in Sharm. Do you want to add?');
+      if (question == true) {
+        alert('You add to Sharm group');
+        sharm = sharm - numberMemmbers;
+        console.log(`Free Places in Sharm: ${sharm}`);
+        return;
+      }
+    }
+
+    if (numberMemmbers <= hurgada) {
+      let question = confirm('There are empty seats in Hurgada. Do you want to add?');
+      if (question == true) {
+        alert('You add to Hurgada group');
+        hurgada = hurgada - numberMemmbers;
+        console.log(`Free Places in Hurgada: ${hurgada}`);
+        return;
+      }
+    }
+
+    alert('There are no seats!');
+
+  } else if (numberMemmbers == '') {
+    alert('You must enter the number of members!');
+  } else {
+    alert('Incorrect value!');
   }
 }
+
+main();
--- module-2/main/js/script.js
+++ module-2/main/js/script.js
@@ -1,10 +1,12 @@
+"use strict";
+
 const taba = 1;
 const sharm = 2;
 const hurgada = 3;
 
 const choiceResort = prompt(`
   Choose a resort.
-  Taba - 1. Sharm - 2. Hurgada - 3.'
+  Taba - 1 Sharm - 2 Hurgada - 3
 `);
 
 if (choiceResort == taba) {
