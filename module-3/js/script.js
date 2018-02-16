5b6ab9e5f2d3645881d07b32387403beb97aedf8
Remas Ulianytskiy <remas.omar.101@gmail.com>
2018-02-16 19:54:56 +0200

    add HW module-2 n3

---
+++ module-3/js/script.js
@@ -0,0 +1,19 @@
+const firstRowKeyboard = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'];
+const secondRowKeyboard = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\''];
+const thirdRowKeyboard = ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '\"'];
+
+const keyboard = firstRowKeyboard.concat(secondRowKeyboard, thirdRowKeyboard);
+
+console.log(find(firstRowKeyboard, "q"));
+
+
+
+
+
+
+
+
+
+
+//
+// console.log(keyboard);
