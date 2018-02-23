bce9fece864e1f2f501bef172447e4444c02a73a
Remas Ulianytskiy <remas.omar.101@gmail.com>
2018-02-23 21:18:47 +0200

    add HW module-5 n1

--- module-5/js/script.js
+++ module-5/js/script.js
@@ -1,9 +1,10 @@
 const keyboard = {
   layouts: {
     en: {
-      topRow: ['qwertyuiop'],
-      middleRow: ['asdfghjkl'],
-      bottomRow: ['zxcvbnm']
+      alphabet: 'qwertyuiopasdfghjklzxcvbnm';
+      topRow: [alphabet.slice(0, 10)],
+      middleRow: [alphabet.slice(10, 19)],
+      bottomRow: [alphabet.slice(19, 28);]
     },
 
     ru: {
@@ -23,25 +24,19 @@ const keyboard = {
   currentLang: ''
 };
 
-// cycle start
 
+
+
+
+// cycle start
 do {
   const languageChoice = prompt(`
     Please choose a language.
     En - 0, Ru - 1, Ua - 2
   `)
 
-  if (languageChoice === '0') {
-    alert('0');
-    break
-  } else if (languageChoice === '1') {
-    alert('1');
-    break
-  } else if (languageChoice === '2') {
-    alert('2');
-    break
-  } else {
-    alert(`Inaccessible language selected. Try again.`)
-  }
+  const selectedOption = ['En', 'Ru', 'Ua']
 
-} while (languageChoice != false);
+  alert(`you chouse ${selectedOption[parseInt(languageChoice)]}`)
+
+} while (languageChoice !== '0' && languageChoice !== '1' && languageChoice !== '2'); // условие ?
